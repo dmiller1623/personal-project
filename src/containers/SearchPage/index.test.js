ed5a3300b0d3cfa5a15001788816a8d9e86ec6b5
Dennis <dmiller1623@gmail.com>
2018-09-15 11:41:00 -0600

    Add search page testing

--- src/containers/SearchPage/index.test.js
+++ src/containers/SearchPage/index.test.js
@@ -10,7 +10,33 @@ describe('SearchPage', () => {
     expect(wrapper).toMatchSnapshot()
   })
 
-  it('should call ')
+  it('should call handle submit when a letter is pressed', () => {
+    let eventObject = { target: { name: 'search', value: 'a' } }
+    wrapper = shallow(<SearchPage store={{}}/>)
+
+    wrapper.find('input').simulate('change', eventObject)
+    expect(wrapper.state('search')).toEqual('a')
+  })
+
+  // it('should call search related when clicking on submit', () => {
+  //   let searchRelatedMock = jest.fn()
+  //   wrapper = shallow(<SearchPage searchRelated={searchRelatedMock}/>)
+
+  //   wrapper.find('button').simulate('click')
+  //   expect(searchRelatedMock).toHaveBeenCalled()
+
+  // })
+
+  it('should call addToResources when add resources is called', () => {
+    let mockResource = { name: 'jim morrison' }
+    let mockAddtionalResources = []
+    let mockAddToResources = jest.fn()
+    wrapper = shallow(<SearchPage additionalResources={mockAddtionalResources} addToResources={mockAddToResources}/>)
+    wrapper.instance().addResources(mockResource)
+
+    expect(mockAddToResources).toHaveBeenCalledWith(mockResource)
+
+  })
 
   describe('mapStateToProps', () => {
     it('should return an array of related items', () => {
