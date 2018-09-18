be4c44d9a4124695a79440eb31d6e3d3b19a0730
Dennis <dmiller1623@gmail.com>
2018-09-18 08:48:36 -0600

    Add fourth log

--- src/utilities/apiCalls/apiCalls.js
+++ src/utilities/apiCalls/apiCalls.js
@@ -4,7 +4,7 @@ import { cleanData } from '../helper/helper.js'
 export const getRelatedData = async (search) => {
  const key = process.env.API_KEY
  const url = `https://tastedive.com/api/similar?q=${search}&k=${key}&info=1`
- console.log(key)
+ console.log(API_KEY)
  console.log(search)
  console.log(url)
   try {
