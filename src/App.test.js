d334c66cbddfdaa0c173bbf0315f40a5cc358d94
Dennis <dmiller1623@gmail.com>
2018-09-18 14:12:05 -0600

    Remove commented out code

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import { BrowserRouter } from 'react-router-dom';
 import { mapStateToProps, App } from './App.js'
-import { searchRelated } from './actions/index.js';
 
 describe('App component', () => {
   it('should match the snapshot', () => {
@@ -13,6 +12,7 @@ describe('App component', () => {
 
     expect(wrapper).toMatchSnapshot();
   });
+  
   describe('mapStateToProps', () => {
     it('should return an array of additional resources', () => {
 
